f31f86f7fc276083544dd5ee6ca9ec00b1222cc0
Melanie Zhang <melanizhang@gmail.com>
2022-03-11 13:43:18 -0800

    adaptive learning rate

--- train.py
+++ train.py
@@ -71,7 +71,7 @@ def main(args):
     # Get optimizer and scheduler
     optimizer = optim.Adadelta(model.parameters(), args.lr,
                                weight_decay=args.l2_wd)
-    scheduler = sched.LambdaLR(optimizer, lambda s: 1.)  # Constant LR
+    scheduler = sched.StepLR(optimizer, step_size=5, gamma=0.1)  # Constant LR
 
     # Get data loader
     log.info('Building dataset...')
